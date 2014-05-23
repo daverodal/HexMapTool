79b9e83bacef154abb3cd281e4cf24ca5c908276
daverodal <dave.rodal@gmail.com>
2014-05-23 09:09:56 -0400

    made map root directory configurable

--- rest/index.php
+++ rest/index.php
@@ -1,10 +1,11 @@
 <?php
+define("MAPROOT","/var/maproot");
 if($_SERVER['REQUEST_METHOD'] == 'PUT'){
     $putdata = fopen("php://input", "r");
     $pInfo = $_SERVER['PATH_INFO'];
 
     /* Open a file for writing */
-    $fp = fopen("/tmp$pInfo", "w");
+    $fp = fopen(MAPROOT."$pInfo", "w");
 
     /* Read the data 1 KB at a time
        and write to the file */
@@ -21,7 +22,7 @@ if($_SERVER['REQUEST_METHOD'] == 'PUT'){
     $pName = preg_replace("/\//","",$pName);
     $pName = preg_replace("/maps/","map",$pName);
     $pName = preg_replace("/hexes/","hex",$pName);
-    $ret = json_decode(file_get_contents("/tmp$pInfo"));
+    $ret = json_decode(file_get_contents(MAPROOT."$pInfo"));
     $ret->$pName->id = $id;
     header('Content-type: application/json');
     echo json_encode($ret);
@@ -29,13 +30,13 @@ if($_SERVER['REQUEST_METHOD'] == 'PUT'){
 }
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $pInfo = $_SERVER['PATH_INFO'];
-    $id = file_get_contents("/tmp$pInfo/_id");
-    file_put_contents("/tmp$pInfo/_id",$id+1);
+    $id = file_get_contents(MAPROOT."$pInfo/_id");
+    file_put_contents(MAPROOT."$pInfo/_id",$id+1);
     $id = trim($id);
     $putdata = fopen("php://input", "r");
 
     /* Open a file for writing */
-    $fp = fopen("/tmp$pInfo/$id", "w");
+    $fp = fopen(MAPROOT."$pInfo/$id", "w");
 
     /* Read the data 1 KB at a time
        and write to the file */
@@ -48,7 +49,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     /* Close the streams */
     fclose($fp);
     fclose($putdata);
-    $ret = json_decode(file_get_contents("/tmp$pInfo/$id"));
+    $ret = json_decode(file_get_contents(MAPROOT."$pInfo/$id"));
     $ret->$pName->id = $id;
     header('Content-type: application/json');
     echo json_encode($ret);
@@ -69,7 +70,7 @@ if($_SERVER['REQUEST_METHOD'] == 'GET'){
     $ret = array();
     $singleModel = substr($modelName,0, -1);
     if($id){
-        $model = json_decode(file_get_contents("/tmp/$modelName/$id"));
+        $model = json_decode(file_get_contents(MAPROOT."/$modelName/$id"));
         $model->$singleModel->id = $id;
 //        var_dump($model);
         $top->$singleModel = $model->$singleModel;
@@ -77,7 +78,7 @@ if($_SERVER['REQUEST_METHOD'] == 'GET'){
         echo json_encode($top);
         exit();
     }
-    if ($handle = opendir("/tmp/$modelName")) {
+    if ($handle = opendir(MAPROOT."/$modelName")) {
 
 
         /* This is the correct way to loop over the directory. */
@@ -85,7 +86,7 @@ if($_SERVER['REQUEST_METHOD'] == 'GET'){
             if($entry === "." || $entry === ".." || $entry === "_id"){
                 continue;
             }
-            $model = json_decode(file_get_contents("/tmp/$modelName/$entry"));
+            $model = json_decode(file_get_contents(MAPROOT."/$modelName/$entry"));
             $model->$singleModel->id = $entry;
             $ret[] = $model->$singleModel;
         }
@@ -158,7 +159,7 @@ if (false){
         $ret->post = $obj;
         echo json_encode($ret);
         exit();
-        echo file_get_contents("/tmp/$pInfo");
+        echo file_get_contents(MAPROOT."/$pInfo");
         exit();
     }
     $obj = json_decode(file_get_contents("http://127.0.0.1:5984/posts/_design/posts/_view/posts"));
@@ -176,7 +177,7 @@ if (false){
 
     $posts = $obj->rows;
     $posts = array();
-    $directory = "/tmp/posts";
+    $directory = MAPROOT."/posts";
     $iterator = new DirectoryIterator($directory);
     foreach ($iterator as $fileinfo) {
         if ($fileinfo->isFile()) {
